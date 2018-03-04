2847742391388f0b85ee4b17065a9544a45dfccf
Jonathan Rudenberg <jonathan@titanous.com>
2018-03-03 20:53:48 -0500

    Fix bypass
    This fixes a trivial bypass via `delete navigator.usb`
    
    Originally reported at: https://twitter.com/jonathansampson/status/969976922829676548

--- disable_webusb.js
+++ disable_webusb.js
@@ -1,3 +1,9 @@
 var script = document.createElement('script');
-script.text = '{ navigator.__defineGetter__("usb", () => undefined); }';
+script.text = `
+if (navigator.usb) {
+  delete navigator.usb.__proto__.getDevices;
+  delete navigator.usb.__proto__.requestDevice;
+  navigator.__defineGetter__("usb", () => undefined);
+}
+`;
 document.documentElement.appendChild(script);
